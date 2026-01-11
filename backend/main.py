be1a06afdc8538ab816fcf2f9a485891ea2c4113
GavinPinto <gavinpinto620@gmail.com>
2026-01-11 08:28:24 -0500

    whoops typo on server name

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 app = FastAPI()
 
 # 2. CORS Setup
-origins = ["http://localhost:5173", "http://localhost:3000", "https://dh12.vercel.app"]
+origins = ["http://localhost:5173", "http://localhost:3000", "https://dh-12.vercel.app"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
