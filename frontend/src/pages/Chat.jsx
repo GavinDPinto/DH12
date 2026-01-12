ee931d23484dd0844877b5bcbc106bd8c6ebb35b
GavinPinto <gavinpinto620@gmail.com>
2026-01-11 19:53:36 -0500

    change chatbot section text and placeholder

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -15,14 +15,15 @@ export default function Chat({ onSend }) {
 
   return (
     <div>
-    <div className="w-full text-center mt-40 mb-10 text-6xl font-sans font-bold text-white">How can I help you today?</div>
+    <div className="w-full text-center mt-40 mb-4 text-6xl font-sans font-bold text-white">Add a goal. We'll give you tasks.</div>
+    <div className="w-full text-center mb-10 text-lg text-gray-400">Make it as vague or as specific as you want</div>
     <div className="w-full flex items-center p-2 bg-gray-900 rounded-xl shadow-md">
       <input
         type="text"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={handleKeyPress}
-        placeholder="Ask me anything..."
+        placeholder="e.g., 'I wanna workout more', 'Learn to code', 'Read more books'..."
         className="flex-1 bg-gray-800 text-white placeholder-gray-400 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <button
